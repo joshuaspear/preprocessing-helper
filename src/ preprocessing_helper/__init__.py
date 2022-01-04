7cf80b4b2fe74a1493f05c05662c24bdab0dc922
Joshua Spear <josh.spear9@gmail.com>
2022-01-04 15:49:09 +0000

    made classes available in init

--- src/ preprocessing_helper/__init__.py
+++ src/ preprocessing_helper/__init__.py
@@ -1,4 +1,6 @@
 import logging
+from .FileNameGenerator import FileNameGenerator
+from .RawDataLoader import RawDataLoader
 
 logger = logging.getLogger("preprocessing_helper")
 console_handler = logging.StreamHandler()
