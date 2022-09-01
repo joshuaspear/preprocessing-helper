34e05a5f4c17024351ac033ad5a526193f92b221
Joshua Spear <josh.spear9@gmail.com>
2022-09-01 14:16:23 +0100

    incremented version for pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name='preprocessing_helper',
-    version='1.2.2',
+    version='1.2.3',
     description="",
     long_description=README,
     packages=setuptools.find_packages(where="src"),
